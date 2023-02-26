56b67eac6d8268be772e9cd643793fd9d1656300
Ryan Belgrave <ryan@belgrave.me>
2023-02-26 12:35:38 -0600

    add prom and use tailscale serve instead of certs

--- roles/grafana/templates/var/ssl/grafana/cert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -euo pipefail
-
-/usr/local/bin/tailscale cert --cert-file /var/ssl/grafana/tls.crt --key-file /var/ssl/grafana/tls.key {{ inventory_hostname }}
-/usr/sbin/chown grafana:grafana /var/ssl/grafana/tls.crt /var/ssl/grafana/tls.key
-/bin/chmod 0600 /var/ssl/grafana/tls.crt /var/ssl/grafana/tls.key
-/usr/sbin/service grafana restart
